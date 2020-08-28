ccd16806428858ec9e3b763109e3965b94cd54a8
Mel <gonzalezyescas@gmail.com>
2020-08-28 01:49:33 -0500

    inicializando el proyecto

---
+++ clase1.js
@@ -0,0 +1,9 @@
+console.log('Hello world JS');
+var name;
+name = 'Mely';
+var age = 22;
+console.log(`Hola ${name} tienes ${age} años`);
+console.log('\nhola ' + name + 'tienes ' + age )
+var uppercaseName = name.toUpperCase();
+
+console.log(`${uppercaseName.charAt(uppercaseName.length-1)}`)
