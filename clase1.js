e77e9fe669a1b6a328354918cea45b1622bb17b5
Mariel Gonzalez <gonzalezyescas@gmail.com>
2020-08-29 00:30:22 -0500

    diferentes formas de imprimir en un objet

--- clase1.js
+++ clase1.js
@@ -7,3 +7,27 @@ console.log('\nhola ' + name + 'tienes ' + age )
 var uppercaseName = name.toUpperCase();
 
 console.log(`${uppercaseName.charAt(uppercaseName.length-1)}`)
+
+var person ={
+    name : 'Mel',
+    age : 22,
+    last : 'yesi'
+}
+
+function impr (person){
+    console.log(person.name.toUpperCase())
+}
+
+impr(person)
+
+function imprim ({name }){
+    console.log(name.toUpperCase())
+}
+imprim(person)
+
+
+function impri (name){
+    console.log(name.toUpperCase());
+}
+
+impri(person.name);
